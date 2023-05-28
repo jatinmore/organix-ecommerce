d411f0aace608ca3beb0a737bd650c7aac88c025
jatin more <jatinmore077@gmail.com>
2023-05-28 15:08:39 +0530

    imports path updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { Routes, Route, NavLink } from "react-router-dom";
-import { Home } from "./pages/Home";
-import { Product } from "./pages/Product";
-import { Cart } from "./pages/Cart";
-import { Wishlist } from "./pages/Wishlist";
-import { ProductDetail } from "./pages/ProductDetail";
-import { Login } from "./pages/LoginSignup";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home/Home";
+import { ProductDetail } from "./pages/ProductCard/ProductDetail";
+import { Product } from "./pages/Product/Product";
+import { Cart } from "./pages/Cart/Cart";
+import { Wishlist } from "./pages/Wishlist/Wishlist";
+import { Login } from "./pages/LoginSignup/LoginSignup";
 import "./App.css";
 import { Navbar } from "./components/Navbar/Navbar";
 
@@ -13,13 +13,13 @@ function App() {
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="product" element={<Product />} />
-        <Route path="cart" element={<Cart />} />
-        <Route path="/wishlist" element={<Wishlist />} />
-        <Route path="/productDetail" element={<ProductDetail />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+        <Route path="/" element={<Home />} />{" "}
+        <Route path="product" element={<Product />} />{" "}
+        <Route path="cart" element={<Cart />} />{" "}
+        <Route path="/wishlist" element={<Wishlist />} />{" "}
+        <Route path="/productDetail" element={<ProductDetail />} />{" "}
+        <Route path="/login" element={<Login />} />{" "}
+      </Routes>{" "}
     </div>
   );
 }
