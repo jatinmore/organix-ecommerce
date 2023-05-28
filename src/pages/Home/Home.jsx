2224503ebb62c3055e3570c051ffe54986421e62
jatin more <jatinmore077@gmail.com>
2023-05-28 17:09:47 +0530

    home page banner added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useEffect } from "react";
-
+import img2 from "../../assets/OFFER.png";
+import img1 from "../../assets/banner.png";
 export const Home = () => {
   const [data, setData] = useState([]);
   const getData = async () => {
@@ -17,15 +18,26 @@ export const Home = () => {
     getData();
   }, []);
   return (
-    <div className="container">
-      <div className="grid-container">
-        {data.map(({ categoryName, id, img }) => (
-          <div key={id} className={id}>
-            <img className="avatar-img-lg" src={img} alt="product-category" />
-            <p>{categoryName}</p>
-          </div>
-        ))}
+    <>
+      <div className="container">
+        <div className="grid-container">
+          {data.map(({ categoryName, id, img }) => (
+            <div key={id} className={id}>
+              <img className="avatar-img-lg" src={img} alt="product-category" />
+              <p>{categoryName}</p>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+
+      <div className="items">
+        <div className="banner">
+          <img src={img1} className="img-responsive " alt="" />
+        </div>
+        <div className="ad">
+          <img src={img2} className="img-responsive" alt="" />
+        </div>
+      </div>
+    </>
   );
 };
