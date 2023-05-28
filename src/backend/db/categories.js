85d1bf096ceba9407829c88104f8a7201e55db92
jatin more <jatinmore077@gmail.com>
2023-05-28 17:03:18 +0530

    categories data added to db and fetch call added to homepage

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -5,23 +5,42 @@ import { v4 as uuid } from "uuid";
  * You can add category of your wish with different attributes
  * */
 
-export const categories = [
-  {
-    _id: uuid(),
-    categoryName: "fiction",
-    description:
-      "literature in the form of prose, especially novels, that describes imaginary events and people",
-  },
-  {
-    _id: uuid(),
-    categoryName: "non-fiction",
-    description:
-      "Non-fiction is writing that gives information or describes real events, rather than telling a story.",
-  },
-  {
-    _id: uuid(),
-    categoryName: "horror",
-    description:
-      "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
-  },
+export const categories = [{
+        _id: uuid(),
+        id: "item1",
+        categoryName: "Fruit",
+        img: "https://img.freepik.com/free-photo/grapes-strawberries-pineapple-kiwi-apricot-banana-whole-pineapple_23-2147968680.jpg?w=900",
+
+    },
+
+    {
+        _id: uuid(),
+        id: "item2",
+        categoryName: "Vegetables",
+        img: "https://img.freepik.com/free-photo/top-view-assortment-vegetables-paper-bag_23-2148853335.jpg?w=996"
+    },
+
+    {
+        _id: uuid(),
+        id: "item3",
+        categoryName: "Dry Fruits",
+        img: "https://img.freepik.com/free-photo/set-pecan-pistachios-almond-peanut-cashew-pine-nuts-lined-up-assorted-nuts-dried-fruits-mini-different-bowls_176474-2051.jpg?w=900",
+
+    },
+
+    {
+        _id: uuid(),
+        id: "item4",
+        categoryName: "Dairy",
+        img: "https://img.freepik.com/free-photo/dairy-products_114579-8756.jpg?w=360",
+
+    },
+
+    {
+        _id: uuid(),
+        id: "item5",
+        categoryName: "Fresh Juices",
+        img: "https://img.freepik.com/free-photo/front-view-fresh-fruit-cocktails-with-fresh-fruit-slices-ice-cooling-blue-drink-juice-cocktail-fruit-color_140725-24741.jpg?w=996",
+
+    },
 ];
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,31 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
 export const Home = () => {
-  return <div className="container"></div>;
+  const [data, setData] = useState([]);
+  const getData = async () => {
+    try {
+      const response = await fetch("/api/categories");
+      const data = await response.json();
+      setData(data.categories);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <div className="container">
+      <div className="grid-container">
+        {data.map(({ categoryName, id, img }) => (
+          <div key={id} className={id}>
+            <img className="avatar-img-lg" src={img} alt="product-category" />
+            <p>{categoryName}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
